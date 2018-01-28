5ccc6881c4d4d16c3b3d14e86fcf552c77fe6c7b
Jim Publicover <jameslpublicover@gmail.com>
2018-01-28 14:15:27 -0500

    authenticate web token

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -7,12 +7,12 @@ module ExceptionHandler
   class InvalidToken < StandardError; end
 
   # define custom handlers
-  rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
-  rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
-  rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
-  rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
-
   included do
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
     end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :user do
     name { Faker::Name.name }
     email 'name@place.com'
-    password password
+    password 'password'
   end
 end
