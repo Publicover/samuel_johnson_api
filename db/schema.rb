a110bab0b097cab55c6145b2702be4cefdfb6928
Jim Publicover <jameslpublicover@gmail.com>
2018-02-01 14:33:16 -0500

    new pg version up

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,15 @@
 
 ActiveRecord::Schema.define(version: 20180127212153) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "definitions", force: :cascade do |t|
     t.string   "name"
     t.integer  "word_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["word_id"], name: "index_definitions_on_word_id"
+    t.index ["word_id"], name: "index_definitions_on_word_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +37,5 @@ ActiveRecord::Schema.define(version: 20180127212153) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "definitions", "words"
 end
